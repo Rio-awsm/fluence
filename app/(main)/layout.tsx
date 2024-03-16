e30a70ea3a215727ae8d5a9b20fb4bf7cd1d71e0
Supriyo Saha <mm.saha1982@gmail.com>
2024-03-17 02:52:08 +0530

    learn page layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,10 +8,10 @@ type Props = {
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-    <MobileHeader />
-    <Sidebar className="hidden lg:flex" />
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
-        <div className="max-w-[1056px] mx-auto pt-6 h-full bg-red-700">{children}</div>
+        <div className="max-w-[1056px] mx-auto pt-6 h-full">{children}</div>
       </main>
     </>
   );
