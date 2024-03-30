439d36d5b5d9d82812f2b56407bca97ca4e0eefb
Supriyo Saha <mm.saha1982@gmail.com>
2024-03-30 14:36:23 +0530

    edit course

---
+++ app/admin/app.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Admin, ListGuesser, Resource } from "react-admin";
+import simpleRestProvider from "ra-data-simple-rest";
+import { CourseList } from "./course/list";
+import { CourseCreate } from "./course/create";
+import { CourseEdit } from "./course/edit";
+
+const dataProvider = simpleRestProvider("/api");
+
+const App = () => {
+  return (
+    <Admin dataProvider={dataProvider}>
+      <Resource
+        name="courses"
+        recordRepresentation="title"
+        list={CourseList}
+        create={CourseCreate}
+        edit={CourseEdit}
+      />
+    </Admin>
+  );
+};
+
+export default App;
